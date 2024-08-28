06fc570e26826b70152fda54e9a9ea199b945318
its-rajesh-smp <its.rajeshsmp@gmail.com>
2024-08-28 07:21:45 +0530

    ADD: Added taskType

--- src/pages/practice-set-tasks/components/CreateNewTaskModal.tsx
+++ src/pages/practice-set-tasks/components/CreateNewTaskModal.tsx
@@ -3,11 +3,11 @@ import { Button, Dialog, Select, Separator } from "@radix-ui/themes";
 import { useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { useParams } from "react-router-dom";
-import Editor from "../../../components/editor/Editor";
 import Input from "../../../components/inputs/Input";
 import SelectInput from "../../../components/inputs/SelectInput";
 import Modal from "../../../components/others/Modal";
 import { Visibility } from "../../../constants/feed.const";
+import { taskTypes } from "../../../constants/task.const";
 import { CREATE_PRACTICE_TASK } from "../../../graphql/practice/practiceTask.graphql";
 import { useAppDispatch } from "../../../hooks/useAppDispatch";
 import { addPracticeSetTask } from "../../../store/practiceSetTask/practiceSetTaskSlice";
@@ -21,6 +21,7 @@ const initialTaskInputValue = {
   title: "",
   description: "",
   visibility: Visibility.PUBLIC,
+  taskType: taskTypes.RESOURCE,
 };
 
 function CreateNewTaskModal({ setOpen, open }: ICreateNewTaskModalProps) {
@@ -78,37 +79,44 @@ function CreateNewTaskModal({ setOpen, open }: ICreateNewTaskModalProps) {
             label="Title"
           />
 
-          <div className="small_scrollbar flex h-32 flex-col !gap-1 overflow-y-auto">
-            <label className="text-sm font-medium">Description</label>
-            <Editor
-              onChange={(value) => {
-                setTaskInput((prev) => ({
-                  ...prev,
-                  description: JSON.stringify(value),
-                }));
-              }}
-              className="w-full"
-            />
-          </div>
-
           <div className="flex items-center justify-between">
-            <div className="flex items-center gap-1">
-              <label htmlFor="visibility" className="text-xs">
-                Visibility:{" "}
-              </label>
-              <SelectInput
-                className="flex gap-3 rounded-full bg-[#f9fcff] px-2 py-1 text-sm"
-                defaultValue={taskInput.visibility}
-                onValueChange={(value) => {
-                  setTaskInput((prev) => ({ ...prev, visibility: value }));
-                }}
-              >
-                {Object.entries(Visibility).map(([key, value]) => (
-                  <Select.Item key={key} value={value}>
-                    {value}
-                  </Select.Item>
-                ))}
-              </SelectInput>
+            <div className="flex flex-row gap-3">
+              <div className="flex items-center gap-1">
+                <label htmlFor="visibility" className="text-xs">
+                  Visibility:{" "}
+                </label>
+                <SelectInput
+                  className="flex gap-3 rounded-full bg-[#f9fcff] px-2 py-1 text-sm"
+                  defaultValue={taskInput.visibility}
+                  onValueChange={(value) => {
+                    setTaskInput((prev) => ({ ...prev, visibility: value }));
+                  }}
+                >
+                  {Object.entries(Visibility).map(([key, value]) => (
+                    <Select.Item key={key} value={value}>
+                      {value}
+                    </Select.Item>
+                  ))}
+                </SelectInput>
+              </div>
+              <div className="flex items-center gap-1">
+                <label htmlFor="taskType" className="text-xs">
+                  Task Type:{" "}
+                </label>
+                <SelectInput
+                  className="flex gap-3 rounded-full bg-[#f9fcff] px-2 py-1 text-sm"
+                  defaultValue={taskTypes.RESOURCE}
+                  onValueChange={(value) => {
+                    setTaskInput((prev) => ({ ...prev, taskType: value }));
+                  }}
+                >
+                  {Object.entries(taskTypes).map(([key, value]) => (
+                    <Select.Item key={key} value={value}>
+                      {value}
+                    </Select.Item>
+                  ))}
+                </SelectInput>
+              </div>
             </div>
             <Button
               onClick={handelCreateTask}
