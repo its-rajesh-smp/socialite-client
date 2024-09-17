27f39196b1ce38fa58f383557090c18c530e2572
its-rajesh-smp <its.rajeshsmp@gmail.com>
2024-09-17 17:38:19 +0530

    UI-FIX: Refactored Create Practice Set Modal

--- src/pages/practice/components/NewPracticeModal.tsx
+++ src/pages/practice/components/NewPracticeModal.tsx
@@ -1,14 +1,25 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { useMutation } from "@apollo/client";
-import { Dialog, Select, Separator } from "@radix-ui/themes";
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import { IoCloseSharp } from "react-icons/io5";
-import Button from "../../../components/inputs/Button";
 import Input from "../../../components/inputs/Input";
-import SelectInput from "../../../components/inputs/SelectInput";
-import Modal from "../../../components/others/Modal";
+import { Visibility } from "../../../constants/common.const";
 import { CREATE_PRACTICE_SET } from "../../../graphql/practice/practiceSet.graphql";
 import { IPracticeSet } from "../../../types/practice";
-import { Visibility } from "../../../constants/common.const";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectValue,
+  SelectTrigger,
+} from "@/components/ui/select";
 
 interface INewPracticeModalProps {
   open: boolean;
@@ -55,69 +66,77 @@ function NewPracticeModal({
   };
 
   return (
-    <Modal open={open}>
-      <Dialog.Title className="flex items-center p-4">
-        <p className="w-full text-center font-bold text-primary">
-          Create Practice Set
-        </p>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Practice Set</DialogTitle>
+          <DialogDescription>
+            For practice your module you need to create a new practice set.
+            Inside that u will be able to create new tasks.
+          </DialogDescription>
+        </DialogHeader>
 
-        <Button type="iconButton" onClick={() => setOpen(false)}>
-          <IoCloseSharp className="cursor-pointer text-2xl" />
-        </Button>
-      </Dialog.Title>
-      <Separator />
-      <form className="flex flex-col gap-4 p-4">
-        <Input
-          value={practiceSetInput.title}
-          containerClassName="gap-1"
-          label="Practice Set Name"
-          placeholder="ex. DSA practice"
-          onChange={(e) =>
-            setPracticeSetInput((prev) => ({ ...prev, title: e.target.value }))
-          }
-        />
-        <Input
-          value={practiceSetInput.description}
-          containerClassName="gap-1"
-          inputType="text-area"
-          label="Description"
-          placeholder="ex. This is a practice set for DSA"
-          onChange={(e) =>
-            setPracticeSetInput((prev) => ({
-              ...prev,
-              description: e.target.value,
-            }))
-          }
-        />
-        <div className="flex justify-between">
-          <div className="flex items-center gap-1">
-            <label htmlFor="visibility" className="text-xs">
-              Visibility:{" "}
-            </label>
-            <SelectInput
-              className="flex gap-3 rounded-full bg-[#f9fcff] px-2 py-1 text-sm"
+        <form className="flex flex-col gap-4">
+          <Input
+            value={practiceSetInput.title}
+            containerClassName="gap-1"
+            label="Practice Set Name"
+            placeholder="ex. DSA practice"
+            onChange={(e) =>
+              setPracticeSetInput((prev) => ({
+                ...prev,
+                title: e.target.value,
+              }))
+            }
+          />
+          <Input
+            value={practiceSetInput.description}
+            containerClassName="gap-1"
+            inputType="text-area"
+            label="Description"
+            placeholder="ex. This is a practice set for DSA"
+            onChange={(e) =>
+              setPracticeSetInput((prev) => ({
+                ...prev,
+                description: e.target.value,
+              }))
+            }
+          />
+          <div className="flex justify-between">
+            <Select
               defaultValue={practiceSetInput.visibility}
               onValueChange={(value) =>
-                setPracticeSetInput((prev) => ({ ...prev, visibility: value }))
+                setPracticeSetInput((prev) => ({
+                  ...prev,
+                  visibility: value,
+                }))
               }
             >
-              {Object.entries(Visibility).map(([key, value]) => (
-                <Select.Item key={key} value={value}>
-                  {value}
-                </Select.Item>
-              ))}
-            </SelectInput>
+              <SelectTrigger className="w-fit">
+                <SelectValue placeholder="Select visibility" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  {Object.entries(Visibility).map(([key, value]) => (
+                    <SelectItem key={key} value={value}>
+                      {value}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+            <Button
+              loading={createPracticeSetLoading}
+              onClick={handelCreatePracticeSet}
+              className="cursor-pointer rounded-md px-2 py-1"
+            >
+              Create
+            </Button>
           </div>
-          <Button
-            loading={createPracticeSetLoading}
-            onClick={handelCreatePracticeSet}
-            className="cursor-pointer rounded-md px-2 py-1"
-          >
-            Create
-          </Button>
-        </div>
-      </form>
-    </Modal>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
 
